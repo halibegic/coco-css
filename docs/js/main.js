1524cc11eea02bcbc85a0f2555eaea12a8b0fb94
Hasan Alibegić <hasan.alibegic@morescreens.net>
2021-01-18 15:27:24 +0100

    ok

--- docs/js/main.js
+++ docs/js/main.js
@@ -37,28 +37,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     );
 
-    // Tab toggle
-
-    [].forEach.call(
-        document.querySelectorAll("[data-toggle='tab']"),
-        function (el) {
-            el.onclick = function () {
-                var tab = el.closest(".tab");
-                var active = tab.querySelectorAll(
-                    ".tab-link.active, .tab-pane.active"
-                );
-                for (var i = 0; i < active.length; i++) {
-                    active[i].classList.remove("active");
-                }
-                el.classList.add("active");
-                document
-                    .getElementById(el.getAttribute("href").split("#")[1])
-                    .classList.add("active");
-                return false;
-            };
-        }
-    );
-
     // Icons
     feather.replace();
 });
